f50ee33c1b4218f119993464e5bae3a837861634
salas <rafa.salas@gmail.com>
2016-01-07 14:35:41 +0100

    7 Enero

--- app/src/main/java/com/wallpaper/salas/pruebawallpaper/MainActivity.java
+++ app/src/main/java/com/wallpaper/salas/pruebawallpaper/MainActivity.java
@@ -1,7 +1,16 @@
 package com.wallpaper.salas.pruebawallpaper;
 
 import android.support.v7.app.AppCompatActivity;
+
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.content.Intent;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +19,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(com.wallpaper.salas.pruebawallpaper.R.layout.activity_main);
     }
-}
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    /**
+     * react to the user tapping/selecting an options menu item
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_new_thingy:
+                //Toast.makeText(this, "ADD!", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(this, preferencias.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+
+
+    } //end fragment
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/src/main/java/com/wallpaper/salas/pruebawallpaper/preferencias.java
@@ -0,0 +1,28 @@
+package com.wallpaper.salas.pruebawallpaper;
+
+/**
+ * Created by salas on 07/01/2016.
+ */
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+public class preferencias extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new preferenciasFragment()).commit();
+    }
+
+    public static class preferenciasFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferencias);
+        }
+    }
+
+}
